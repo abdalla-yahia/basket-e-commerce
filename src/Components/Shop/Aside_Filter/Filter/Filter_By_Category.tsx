cf03069e7c85a64bb7835288bd39b80da98a76e9
ابو يحيى عبدالله <abdalla-yahia>
2025-08-28 01:46:24 +0300

    Create Products Page Details

--- src/Components/Shop/Aside_Filter/Filter/Filter_By_Category.tsx
+++ src/Components/Shop/Aside_Filter/Filter/Filter_By_Category.tsx
@@ -7,15 +7,15 @@ export default function Filter_By_Category() {
         <Filter_Title title="Product Categories" />
         {/*Category Filter*/}
         <ul className="flex flex-col justify-start items-start gap-2 ">
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Beverages</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Biscuits & Snacks</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Breads & Bakery</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Breakfast & Dairy</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Frozen Foods</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Fruits & Vegetables</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Grocery & Staples</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Household Needs</label></li>
-            <li className="flex justify-start items-start gap-4"><input type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Meats & Seafood</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Beverages</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Biscuits & Snacks</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Breads & Bakery</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Breakfast & Dairy</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Frozen Foods</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Fruits & Vegetables</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Grocery & Staples</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Household Needs</label></li>
+            <li className="flex justify-start items-start gap-4"><input className=" appearance-none bg-white border border-[#D9D9E9]  p-2 hover:bg-primary/50 checked:bg-primary " type="checkbox" name="" id="" /><label className="text-[#71778E] text-[14px] font-[400]" htmlFor="">Meats & Seafood</label></li>
         </ul>
     </div>
   )
