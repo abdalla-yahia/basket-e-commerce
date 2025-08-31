9f17c5a7d903b6de52a21dec647cd8c32916825e
ابو يحيى عبدالله <abdalla-yahia>
2025-08-31 08:34:59 +0300

    Add Reset State To Brand ,Category And User Slice

--- src/Components/Dashboards/Aside/Aside_Dashboard.tsx
+++ src/Components/Dashboards/Aside/Aside_Dashboard.tsx
@@ -7,19 +7,19 @@ export default function Aside_Dashboard({role}:{role:string}) {
   return (
     <aside className="w-[15%]  flex flex-col justify-start items-start rounded   text-[#3E445A] font-[400] text-sm gap-0 border border-[#E4E5EE]">
           {
-            role === 'users' ? (dashboardIcons?.users?.map(icon=>{
-              return <div key={icon?.id} className='w-full hover:bg-primary hover:text-white duration-100 cursor-pointer p-3  flex justify-between items-start gap-3'>
-                <Link href={icon?.href}>{icon?.title}</Link>
-                <Dashboard_Icon icon={icon?.icon as keyof typeof icons}/>
-              </div>
-            })):
+            role === 'users' ? (dashboardIcons?.users?.map(icon=>
+              <Link key={icon?.id} href={icon?.href} className='w-full hover:bg-primary hover:text-white duration-100 cursor-pointer p-3  flex justify-between items-start gap-3'>
+                    {icon?.title}
+                    <Dashboard_Icon icon={icon?.icon as keyof typeof icons}/>
+                </Link>
+            )):
 
-             role === 'admins' ? (dashboardIcons?.admins?.map(icon=>{
-              return <div key={icon?.id} className='w-full hover:bg-primary hover:text-white duration-100 cursor-pointer p-3  flex justify-between items-start gap-3'>
-                <Link href={icon?.href}>{icon?.title}</Link>
-                <Dashboard_Icon icon={icon?.icon as keyof typeof icons}/>
-              </div>
-            })) :''
+             role === 'admins' ? (dashboardIcons?.admins?.map(icon=>
+                <Link key={icon?.id} href={icon?.href} className='w-full hover:bg-primary hover:text-white duration-100 cursor-pointer p-3  flex justify-between items-start gap-3'>
+                    {icon?.title}
+                    <Dashboard_Icon icon={icon?.icon as keyof typeof icons}/>
+                </Link>
+            )) :''
           }
         </aside>
   )
