2a913d7f21f91d1cfe38ce82cc0a64ed3060796d
ابو يحيى عبدالله <abdalla-yahia>
2025-08-31 08:30:49 +0300

    Add Reset State To Brand ,Category And User Slice

--- src/Components/Stok_Products/Stok_Products.tsx
+++ src/Components/Stok_Products/Stok_Products.tsx
@@ -14,7 +14,7 @@ export default function Stok_Products() {
   //Get First Category To Be A Main Category
   const mainCategory = AllCategories?.categories && AllCategories?.categories?.[0] as UpdateCategory
   return (
-    <div className='flex justify-between items-start h-[240px] mt-5 overflow-hidden  rounded-2xl border border-[#E4E5EE]'>
+    <div className='w-full flex justify-between items-start h-[240px] mt-5 overflow-hidden  rounded-2xl border border-[#E4E5EE]'>
         {/*Main Product*/}
         <div className='flex flex-col h-[250px] justify-center items-center p-5 border border-[#E4E5EE]'>
             <Image  src={mainCategory?.image || "https://res.cloudinary.com/dghqvxueq/image/upload/v1756257568/product_9_io0fca.png"} alt={mainCategory?.title as string} width={200} height={200}/>
@@ -24,11 +24,11 @@ export default function Stok_Products() {
             <span className="text-[12px] text-[#202435] font-[300]" style={{lineHeight:'18px',letterSpacing:'-0.1px'}}>{mainCategory?.products?.length} Items</span>
         </div>
         {/*Items Container*/}
-        <div className="flex justify-start items-start flex-wrap">
+        <div className="w-full flex justify-start items-start flex-wrap">
             {/*Items*/}
             {
                 AllCategories?.categories && AllCategories?.categories?.slice(0,8)?.map((category:UpdateCategory)=>
-                    <div key={category?.id} className="w-1/4 h-1/2 flex justify-start items-center gap-2 p-5 border border-[#E4E5EE]">
+                    <div key={category?.id} className="w-1/4 flex-1 h-1/2 flex justify-start items-center gap-2 p-5 border border-[#E4E5EE]">
                         <Image src={category?.image || "https://res.cloudinary.com/dghqvxueq/image/upload/v1756257566/product_10_lnu3vr.png"} alt={category?.title as string} width={80} height={50}/>
                         {/*Content*/}
                         <div className="flex flex-col justify-start items-start gap-1">
