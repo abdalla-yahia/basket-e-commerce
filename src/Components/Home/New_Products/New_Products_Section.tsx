f2da503f8a68fa045a32222773813f9762484ec4
ابو يحيى عبدالله <abdalla-yahia>
2025-09-01 12:32:38 +0300

    Stretch Product Card To Get Full Height

--- src/Components/Home/New_Products/New_Products_Section.tsx
+++ src/Components/Home/New_Products/New_Products_Section.tsx
@@ -31,7 +31,7 @@ export default function New_Products_Section() {
       {/*Products*/}
       <div className="w-full  flex justify-between flex-wrap gap-0 items-start mt-3">
         {
-          AllProducts?.products && AllProducts?.products?.slice(0, 12)?.map((product: UpdateProduct) =>
+          AllProducts?.products && AllProducts?.products?.slice(0, 8)?.map((product: UpdateProduct) =>
             <Product_Card slug={product?.slug as string} key={product?.id} img={product?.image || 'https://res.cloudinary.com/dghqvxueq/image/upload/v1756233979/product_2_kmlstf.png'} offer={product?.offer || '22%'} title={product?.title as string} rating={product?.rating || '4.5'} oldprice={product?.oldPrice as unknown as string} price={product?.price as unknown as string} quantity={product?.quantity as number} />
           )
         }
