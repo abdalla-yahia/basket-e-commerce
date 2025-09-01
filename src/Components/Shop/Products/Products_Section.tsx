b446c18439fe1f4e0cbd4d3ac77604e4fe1d9238
ابو يحيى عبدالله <abdalla-yahia>
2025-09-01 12:38:21 +0300

    Stretch Product Card To Get Full Height

--- src/Components/Shop/Products/Products_Section.tsx
+++ src/Components/Shop/Products/Products_Section.tsx
@@ -14,7 +14,7 @@ export default function Products_Section() {
       <div className="w-full  flex justify-between flex-wrap gap-0 items-start mt-3">
         {
           AllProducts?.products && AllProducts?.products?.map((product:UpdateProduct)=>
-            <Product_Card key={product?.id} slug={product?.slug as string} img={product?.image || 'https://res.cloudinary.com/dghqvxueq/image/upload/v1756233979/product_2_kmlstf.png'} offer={product?.offer || '22%'} title={product?.title as string}  rating={''} oldprice={product?.oldPrice as unknown as string} price={product?.price as unknown as string} quantity={product?.quantity as number} iscounter/>
+            <Product_Card key={product?.id} slug={product?.slug as string} img={product?.image || 'https://res.cloudinary.com/dghqvxueq/image/upload/v1756233979/product_2_kmlstf.png'} offer={product?.offer as string} title={product?.title as string}  rating={''} oldprice={product?.oldPrice as unknown as string} price={product?.price as unknown as string} quantity={product?.quantity as number} iscounter/>
           )
         }
       </div>
