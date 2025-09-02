8d22d004f87219296dc755ad86433081675257e5
ابو يحيى عبدالله <abdalla-yahia>
2025-09-02 21:37:37 +0300

    Filter Products By Categories & Brands & Price

--- src/Components/Shop/Products/Products_Section.tsx
+++ src/Components/Shop/Products/Products_Section.tsx
@@ -5,6 +5,8 @@ import { UpdateProduct } from "@/Interfaces/ProductInterface";
 
 export default function Products_Section({setSearchText}:{setSearchText:(arfg0:string)=>void}) {
   const {ProductsBySearchText} = useAppSelector((state:RootState)=>state.product)
+  const {ProductsByFilter} = useAppSelector((state:RootState)=>state.product)
+  const {AllProducts} = useAppSelector((state:RootState)=>state.product)
   
   return (
     <>
@@ -13,9 +15,23 @@ export default function Products_Section({setSearchText}:{setSearchText:(arfg0:s
       {/*Products*/}
       <div className="w-full  flex justify-between flex-wrap gap-0 items-start mt-3">
         {
+          ProductsBySearchText?.products?.length?
+         ( 
           ProductsBySearchText?.products && ProductsBySearchText?.products?.map((product:UpdateProduct)=>
             <Product_Card key={product?.id} slug={product?.slug as string} img={product?.image || 'https://res.cloudinary.com/dghqvxueq/image/upload/v1756233979/product_2_kmlstf.png'} offer={product?.offer as string} title={product?.title as string}  rating={''} oldprice={product?.oldPrice as unknown as string} price={product?.price as unknown as string} quantity={product?.quantity as number} iscounter/>
           )
+        ):
+        ProductsByFilter?.products?.length ?
+        ( 
+          ProductsByFilter?.products && ProductsByFilter?.products?.map((product:UpdateProduct)=>
+            <Product_Card key={product?.id} slug={product?.slug as string} img={product?.image || 'https://res.cloudinary.com/dghqvxueq/image/upload/v1756233979/product_2_kmlstf.png'} offer={product?.offer as string} title={product?.title as string}  rating={''} oldprice={product?.oldPrice as unknown as string} price={product?.price as unknown as string} quantity={product?.quantity as number} iscounter/>
+          )
+        ):
+        ( 
+          AllProducts?.products && AllProducts?.products?.map((product:UpdateProduct)=>
+            <Product_Card key={product?.id} slug={product?.slug as string} img={product?.image || 'https://res.cloudinary.com/dghqvxueq/image/upload/v1756233979/product_2_kmlstf.png'} offer={product?.offer as string} title={product?.title as string}  rating={''} oldprice={product?.oldPrice as unknown as string} price={product?.price as unknown as string} quantity={product?.quantity as number} iscounter/>
+          )
+        )
         }
       </div>
     </>
