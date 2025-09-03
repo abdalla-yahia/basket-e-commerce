12d233a5614c14805340329d16f2e03555996d59
ابو يحيى عبدالله <abdalla-yahia>
2025-09-03 18:40:54 +0300

    Update Dashboards

--- src/Components/Dashboards/Admins/Admins_Dashboard_Container.tsx
+++ src/Components/Dashboards/Admins/Admins_Dashboard_Container.tsx
@@ -1,13 +1,8 @@
 'use client';
+import { RootState, useAppSelector } from '@/libs/store';
 import * as icon from '@/Utils/Icons/Icons'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie, Cell, Legend } from 'recharts';
 
-const stats = [
-  { title: 'Today Orders', value: 12, icon: '🛒' },
-  { title: 'Active Users', value: 340, icon: '👥' },
-  { title: 'Total Products', value: 129, icon: '📦' },
-  { title: 'Today Sales', value: '4,300 EGP', icon: '💰' },
-];
 
 const salesData = [
   { day: 'Saturday', sales: 1000 },
@@ -19,69 +14,93 @@ const salesData = [
   { day: 'Friday', sales: 2200 },
 ];
 
-const lastOrders = [
-  { id: 'ORD:0123', user: 'Ahmed Ali', total: '450 EGP', status: 'PENDING', date: '2025-08-04' },
-  { id: 'ORD:0124', user: 'Mona Mohamed', total: '800 EGP', status: 'DELIVERED', date: '2025-08-03' },
-  { id: 'ORD:0125', user: 'Khaled Samir', total: '1200 EGP', status: 'CANCELED', date: '2025-08-03' },
-  { id: 'ORD:0126', user: 'Yasser Abdo', total: '300 EGP', status: 'PENDING', date: '2025-08-02' },
-  { id: 'ORD:0127', user: 'Sara Gamal', total: '950 EGP', status: 'DELIVERED', date: '2025-08-01' },
-];
-  const orderStatus = [
-    { name: "Completed", value: 45 },
-    { name: "Pending", value: 35 },
-    { name: "Canceled", value: 18 },
-  ];
-  const COLORS = ["#00C49F", "#FFBB28", "#FF4D4F"];
 
-function StatCard({ title, value, icon }: { title: string; value: string | number; icon: string }) {
-  return (
-    <div className="bg-white dark:bg-gray-900 p-4 rounded shadow flex items-center gap-4">
-      <div className="text-3xl">{icon}</div>
-      <div>
-        <p className="text-gray-500 text-sm">{title}</p>
-        <p className="text-lg font-bold">{value}</p>
-      </div>
-    </div>
-  );
-}
+const orderStatus = [
+  { name: "Completed", value: 45 },
+  { name: "Pending", value: 35 },
+  { name: "Canceled", value: 18 },
+];
+const COLORS = ["#00C49F", "#FFBB28", "#FF4D4F"];
+
 
 export default function Admins_Dashboard_Container() {
+  const { LogedUser } = useAppSelector((state: RootState) => state.auth)
+  const { AllUsers } = useAppSelector((state: RootState) => state.user)
+  const { AllOrders } = useAppSelector((state: RootState) => state.order)
+  const { AllProducts } = useAppSelector((state: RootState) => state.product)
   return (
     <div className="py-6 space-y-6 w-full">
+      {/*Page Title*/}
       <h1 className="text-2xl font-bold text-primary my-3 gap-3 flex justify-start items-center">
-        <icon.IoBarChart className="text-2xl"/>
-        Admin Dashboard {/* {data?.me?.name} */}</h1>
-
+        <icon.IoBarChart className="text-2xl" />
+        Admin Dashboard {LogedUser?.user?.name}</h1>
       {/* Stat cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        {stats.map((stat, i) => (
-          <StatCard key={i} {...stat} />
-        ))}
+        {/*USERS COUNT*/}
+        <div className="bg-white dark:bg-gray-900 p-4 rounded shadow flex items-center gap-4">
+          <div className="text-3xl">
+            <icon.FaUsers className='text-primary' />
+          </div>
+          <div>
+            <p className="text-gray-500 text-sm">Active Users</p>
+            <p className="text-lg font-bold">{AllUsers?.users?.length}</p>
+          </div>
+        </div>
+        {/*ORDERS*/}
+        <div className="bg-white dark:bg-gray-900 p-4 rounded shadow flex items-center gap-4">
+          <div className="text-3xl">
+            <icon.LiaShippingFastSolid className='text-primary' />
+          </div>
+          <div>
+            <p className="text-gray-500 text-sm">All Orders</p>
+            <p className="text-lg font-bold">{AllOrders?.orders?.length}</p>
+          </div>
+        </div>
+        {/*TOTAL PRODUCTS*/}
+        <div className="bg-white dark:bg-gray-900 p-4 rounded shadow flex items-center gap-4">
+          <div className="text-3xl">
+            <icon.FaBoxOpen className='text-primary' />
+          </div>
+          <div>
+            <p className="text-gray-500 text-sm">Total Products</p>
+            <p className="text-lg font-bold">{AllProducts?.FullyProducts?.length}</p>
+          </div>
+        </div>
+        {/*TOTAL SALE*/}
+        <div className="bg-white dark:bg-gray-900 p-4 rounded shadow flex items-center gap-4">
+          <div className="text-3xl">
+            <icon.GrCurrency className='text-primary' />
+          </div>
+          <div>
+            <p className="text-gray-500 text-sm">Total Sales</p>
+            <p className="text-lg font-bold">4,500 USD</p>
+          </div>
+        </div>
       </div>
 
       {/* Chart */}
-        <div className=" rounded-lg shadow p-4 ">
-          <h2 className="text-lg font-semibold mb-4">Orders Status</h2>
-          <ResponsiveContainer width="100%" height={250}>
-            <PieChart>
-              <Pie
-                data={orderStatus}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                outerRadius={80}
-                dataKey="value"
-                label={({ name }) => name}
-              >
-                {orderStatus.map((entry, index) => (
-                  <Cell key={index} fill={COLORS[index % COLORS.length]} />
-                ))}
-              </Pie>
-              <Legend />
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
-        </div>
+      <div className=" rounded-lg shadow p-4 ">
+        <h2 className="text-lg font-semibold mb-4">Orders Status</h2>
+        <ResponsiveContainer width="100%" height={250}>
+          <PieChart>
+            <Pie
+              data={orderStatus}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              outerRadius={80}
+              dataKey="value"
+              label={({ name }) => name}
+            >
+              {orderStatus.map((entry, index) => (
+                <Cell key={index} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Legend />
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
 
       <div className="bg-white dark:bg-gray-900 py-4 rounded shadow w-full">
         <h2 className="text-xl font-semibold mb-4">Weekly Sales</h2>
@@ -105,29 +124,27 @@ export default function Admins_Dashboard_Container() {
               <tr className="bg-gray-100 dark:bg-gray-800">
                 <th className="p-2">Order ID</th>
                 <th className="p-2">Customer</th>
-                <th className="p-2">Total</th>
                 <th className="p-2">Status</th>
                 <th className="p-2">Date</th>
               </tr>
             </thead>
             <tbody>
-              {lastOrders.map((order) => (
+              {AllOrders?.orders?.map((order) => (
                 <tr key={order.id} className="border-b dark:border-gray-700">
                   <td className="p-2">{order.id}</td>
-                  <td className="p-2">{order.user}</td>
-                  <td className="p-2">{order.total}</td>
+                  <td className="p-2">{order.user?.name}</td>
                   <td className="p-2">
-                      {/*Order Status*/}
-                        {
-                        order.status == 'PENDING'   ? <p className='rounded-xl p-0.5 flex justify-center bg-sky-200 text-sky-600'>{order.status?.toLowerCase()}</p>:
-                        order.status == 'PREPARING' ? <p className='rounded-xl p-0.5 flex justify-center bg-blue-300 text-blue-600'>{order.status?.toLowerCase()}</p>:
-                        order.status == 'SHIPPED'   ? <p className='rounded-xl p-0.5 flex justify-center bg-green-200 text-green-600'>{order.status?.toLowerCase()}</p>:
-                        order.status == 'DELIVERED' ? <p className='rounded-xl p-0.5 flex justify-center bg-green-300 text-green-700'>{order.status?.toLowerCase()}</p>:
-                        order.status == 'CANCELED'  ? <p className='rounded-xl p-0.5 flex justify-center bg-red-200 text-black'>{order.status?.toLowerCase()}</p>:
-                        <p className='rounded-xl p-0.5 flex justify-center bg-red-300 text-black'>{order.status?.toLowerCase()}</p>
-                        }
+                    {/*Order Status*/}
+                    {
+                      order.status == 'PENDING' ? <p className='rounded-xl p-0.5 flex justify-center bg-sky-200 text-sky-600'>{order.status?.toLowerCase()}</p> :
+                        order.status == 'PREPARING' ? <p className='rounded-xl p-0.5 flex justify-center bg-blue-300 text-blue-600'>{order.status?.toLowerCase()}</p> :
+                          order.status == 'SHIPPED' ? <p className='rounded-xl p-0.5 flex justify-center bg-green-200 text-green-600'>{order.status?.toLowerCase()}</p> :
+                            order.status == 'DELIVERED' ? <p className='rounded-xl p-0.5 flex justify-center bg-green-300 text-green-700'>{order.status?.toLowerCase()}</p> :
+                              order.status == 'CANCELED' ? <p className='rounded-xl p-0.5 flex justify-center bg-red-200 text-black'>{order.status?.toLowerCase()}</p> :
+                                <p className='rounded-xl p-0.5 flex justify-center bg-red-300 text-black'>{order.status?.toLowerCase()}</p>
+                    }
                   </td>
-                  <td className="p-2">{order.date}</td>
+                  <td className="p-2">{new Date().toLocaleString(('ar-eg'), { year: '2-digit', month: 'short', day: '2-digit' })}</td>
                 </tr>
               ))}
             </tbody>
--- src/Utils/Icons/Icons.ts
+++ src/Utils/Icons/Icons.ts
@@ -1,28 +1,119 @@
-import { TbError404,TbCategoryPlus } from 'react-icons/tb';
+import { TbError404, TbCategoryPlus } from "react-icons/tb";
 import { HiOutlineHandRaised } from "react-icons/hi2";
-import { BiSolidErrorAlt,BiSolidCategory} from "react-icons/bi";
-import { RiArrowRightSLine,RiArrowLeftSLine } from "react-icons/ri";
-import { FaWhatsapp,FaFacebookF,FaPinterestP,FaRegEye,FaRegEyeSlash,FaChartLine,FaAddressCard,FaUsers,FaTrash,FaRegEdit,FaHome,FaBoxOpen,FaShoppingCart,FaHeart } from "react-icons/fa";
-import { IoClose,IoLogoReddit } from "react-icons/io5";
-import { BsHandbag,BsTwitterX } from "react-icons/bs";
+import { BiSolidErrorAlt, BiSolidCategory } from "react-icons/bi";
+import { RiArrowRightSLine, RiArrowLeftSLine } from "react-icons/ri";
+import {
+  FaWhatsapp,
+  FaFacebookF,
+  FaPinterestP,
+  FaRegEye,
+  FaRegEyeSlash,
+  FaChartLine,
+  FaAddressCard,
+  FaUsers,
+  FaTrash,
+  FaRegEdit,
+  FaHome,
+  FaBoxOpen,
+  FaShoppingCart,
+  FaHeart,
+} from "react-icons/fa";
+import { IoClose, IoLogoReddit } from "react-icons/io5";
+import { BsHandbag, BsTwitterX } from "react-icons/bs";
 import { CiHeart } from "react-icons/ci";
-import { PiShareFatThin,PiHandbag } from "react-icons/pi";
+import { PiShareFatThin, PiHandbag } from "react-icons/pi";
 import { SlTag } from "react-icons/sl";
-import { FaPlus,FaMinus,FaLocationDot,FaMapLocationDot } from "react-icons/fa6";
-import { IoLogoInstagram,IoMdCheckmark } from "react-icons/io";
+import {
+  FaPlus,
+  FaMinus,
+  FaLocationDot,
+  FaMapLocationDot,
+} from "react-icons/fa6";
+import { IoLogoInstagram, IoMdCheckmark } from "react-icons/io";
 import { FiPhoneCall } from "react-icons/fi";
-import { MdOutlineLocalPostOffice,MdDeliveryDining,MdDashboard,MdInventory2,MdAddBox,MdAddToQueue } from "react-icons/md";
-import { CiCreditCardOff,CiSettings } from "react-icons/ci";
-import { IoPersonCircleOutline,IoPricetagsSharp,IoBarChart } from "react-icons/io5";
+import {
+  MdOutlineLocalPostOffice,
+  MdDeliveryDining,
+  MdDashboard,
+  MdInventory2,
+  MdAddBox,
+  MdAddToQueue,
+} from "react-icons/md";
+import { CiCreditCardOff, CiSettings } from "react-icons/ci";
+import {
+  IoPersonCircleOutline,
+  IoPricetagsSharp,
+  IoBarChart,
+} from "react-icons/io5";
 import { LuLoader } from "react-icons/lu";
-import { RiCoupon3Fill,RiSearchLine,RiUserHeartLine } from "react-icons/ri";
+import { RiCoupon3Fill, RiSearchLine, RiUserHeartLine } from "react-icons/ri";
 import { SiBrandfolder } from "react-icons/si";
-import { GrDocumentUpdate,GrDocumentDownload } from "react-icons/gr";
-import { MdDelete,MdCategory,MdOutlineLocalShipping } from "react-icons/md";
+import {
+  GrDocumentUpdate,
+  GrDocumentDownload,
+  GrCurrency,
+} from "react-icons/gr";
+import { MdDelete, MdCategory, MdOutlineLocalShipping } from "react-icons/md";
+import { LiaShippingFastSolid } from "react-icons/lia";
 
-export {BiSolidErrorAlt,HiOutlineHandRaised,TbError404,RiArrowLeftSLine,RiArrowRightSLine,FaWhatsapp,IoClose,BsHandbag,CiHeart,GrDocumentUpdate,MdDelete,
-PiShareFatThin,SlTag,FaPlus,FaMinus,FaFacebookF,IoLogoInstagram,BsTwitterX,IoLogoReddit,FaPinterestP,FaLocationDot,FiPhoneCall,MdOutlineLocalPostOffice,
-CiCreditCardOff,IoPersonCircleOutline,FaRegEye,FaRegEyeSlash,CiSettings,LuLoader,FaChartLine,IoPricetagsSharp,FaTrash,FaRegEdit,IoMdCheckmark,FaHome,
-FaAddressCard,FaMapLocationDot,MdDeliveryDining,MdDashboard,FaUsers,MdInventory2,RiCoupon3Fill,MdAddBox,BiSolidCategory,TbCategoryPlus,SiBrandfolder,MdAddToQueue,
-PiHandbag,RiSearchLine,FaBoxOpen,MdCategory,MdOutlineLocalShipping,IoBarChart,GrDocumentDownload,FaShoppingCart,FaHeart,RiUserHeartLine
-}
+export {
+  GrCurrency,
+  BiSolidErrorAlt,
+  HiOutlineHandRaised,
+  TbError404,
+  RiArrowLeftSLine,
+  RiArrowRightSLine,
+  FaWhatsapp,
+  IoClose,
+  BsHandbag,
+  CiHeart,
+  GrDocumentUpdate,
+  MdDelete,
+  PiShareFatThin,
+  SlTag,
+  FaPlus,
+  FaMinus,
+  FaFacebookF,
+  IoLogoInstagram,
+  BsTwitterX,
+  IoLogoReddit,
+  FaPinterestP,
+  FaLocationDot,
+  FiPhoneCall,
+  MdOutlineLocalPostOffice,
+  CiCreditCardOff,
+  IoPersonCircleOutline,
+  FaRegEye,
+  FaRegEyeSlash,
+  CiSettings,
+  LuLoader,
+  FaChartLine,
+  IoPricetagsSharp,
+  FaTrash,
+  FaRegEdit,
+  IoMdCheckmark,
+  FaHome,
+  FaAddressCard,
+  FaMapLocationDot,
+  MdDeliveryDining,
+  MdDashboard,
+  FaUsers,
+  MdInventory2,
+  RiCoupon3Fill,
+  MdAddBox,
+  BiSolidCategory,
+  TbCategoryPlus,
+  SiBrandfolder,
+  MdAddToQueue,
+  PiHandbag,
+  RiSearchLine,
+  FaBoxOpen,
+  MdCategory,
+  MdOutlineLocalShipping,
+  IoBarChart,
+  GrDocumentDownload,
+  FaShoppingCart,
+  FaHeart,
+  RiUserHeartLine,
+  LiaShippingFastSolid,
+};
