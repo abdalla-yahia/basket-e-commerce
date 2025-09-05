135d27697f0587e80f528b8b05925d22c4299650
ابو يحيى عبدالله <abdalla-yahia>
2025-09-05 05:02:27 +0300

    Update Products Sections

--- src/Components/Header/Sub_Nav_Section/Currency_Language.tsx
+++ src/Components/Header/Sub_Nav_Section/Currency_Language.tsx
@@ -4,21 +4,21 @@ export default function Currency_Language() {
     <div className="hidden md:flex gap-3">
                     {/*Selected Curancy*/}
                     <select name="" id=""  >
-                        <option value="USD" selected>USD</option>
-                        <option value="EUR" >EUR</option>
-                        <option value="SAR" >SAR</option>
-                        <option value="EGP" >EGP</option>
-                        <option value="KWD" >KWD</option>
-                        <option value="QAR" >QAR</option>
+                        <option  value="USD" selected>USD</option>
+                        {/* <option disabled value="EUR" >EUR</option>
+                        <option disabled value="SAR" >SAR</option>
+                        <option disabled value="EGP" >EGP</option>
+                        <option disabled value="KWD" >KWD</option>
+                        <option disabled value="QAR" >QAR</option> */}
                     </select>
                     {/*Selected Language*/}
                     <select name="" id=""  >
-                        <option value="English" selected>English</option>
-                        <option value="العربية" >العربية</option>
-                        <option value="Français" >Français</option>
-                        <option value="Español" >Español</option>
-                        <option value="Italiano" >Italiano</option>
-                        <option value="Português" >Português</option>
+                        <option  value="English" selected>English</option>
+                        {/* <option disabled value="العربية" >العربية</option>
+                        <option disabled value="Français" >Français</option>
+                        <option disabled value="Español" >Español</option>
+                        <option disabled value="Italiano" >Italiano</option>
+                        <option disabled value="Português" >Português</option> */}
                     </select>
                 </div>
   )
